1c6d2dac9dd59d1e3190ad748af7e3efba5855a7
Pedro Lucas <pedrolucas.7@hotmail.com>
2022-02-17 20:55:49 -0300

    Setup inicial da aplicação
    Setup inicial da aplicação

---
+++ api-back-end/src/main/java/api/calcados/CalcadosApplication.java
@@ -0,0 +1,13 @@
+package api.calcados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcadosApplication.class, args);
+	}
+
+}
